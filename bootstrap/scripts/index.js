3cdf0456e136a41bf0dc5a951620fbe47f391fe1
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2019-03-20 16:21:10 +0800

    small update

--- bootstrap/scripts/index.js
+++ bootstrap/scripts/index.js
@@ -11,7 +11,7 @@ function scrollTo(element){
     window.scroll({
         behavior:'smooth',
         left:0,
-        top:element.offsetTop
+        top:element.offsetTop - 100
     });
 }
 
@@ -31,3 +31,40 @@ addFriendBtn.addEventListener('click',()=>{
         status = false;
     }
 });
+
+$(window).scroll(function(){
+    let windowPosition = $(window).scrollTop();
+    let hitpoint = windowPosition;
+    // let functionTabsBar = document.getElementById('functionTabs').offsetTop;    
+    // let profile = document.createElement('img');
+    // profile.id = 'profileIMG'
+    // profile.src = '../images/profile.jpg';
+    // profile.className = 'profile__img';
+    let profile = createSmallProfile('profileIMG','../images/profile.jpg','profile__img');
+
+    if( hitpoint > document.getElementById('functionTabs').offsetTop + 150)
+    {
+        // $('#functionTabs').addClass('sticky-top');
+        $('.navbar').addClass('sticky-top');
+        // $('#functionTabs .container').removeClass('py-sm-4');
+        // $('#functionTabs .container').addClass('py-sm-2');        
+    }
+
+    else
+    {
+        // $('#functionTabs .container').removeClass('py-sm-2');
+        // $('#functionTabs .container').addClass('py-sm-4');
+        $('.navbar').removeClass('sticky-top');
+    }
+
+    hitpoint = $('#functionTabs').scrollTop;
+
+});
+
+function createSmallProfile(id, src, className){
+    let smallProfile = document.createElement('img'); 
+    smallProfile.id = id;
+    smallProfile.src = src;
+    smallProfile.className = className;
+    return smallProfile;
+}
